3c110a163b8044cf6cc43c08d9d9b9f7ab9f16b6
jlswift2 <jlswift2@gmail.com>
2021-09-13 16:15:52 +0900

    correct slight naming inconsistencies

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import RoomsList, RoomDetail
+from .views import RoomsList, RoomDetails
 from .apps import RoomsConfig
 
 app_name = RoomsConfig.name
 urlpatterns = [
     path('', RoomsList.as_view(), name='rooms_list'),
-    path('<int:pk>/details', RoomDetail.as_view(), name='room_details')
+    path('<int:pk>/details', RoomDetails.as_view(), name='room_details')
 ]
