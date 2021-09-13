ee1ff01252c54f012665fe3debac45c13e8ac17a
jlswift2 <jlswift2@gmail.com>
2021-09-13 16:13:26 +0900

    add urls for RoomDetail page

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import RoomsList
+from .views import RoomsList, RoomDetail
 from .apps import RoomsConfig
 
 app_name = RoomsConfig.name
 urlpatterns = [
     path('', RoomsList.as_view(), name='rooms_list'),
+    path('<int:pk>/details', RoomDetail.as_view(), name='room_details')
 ]
