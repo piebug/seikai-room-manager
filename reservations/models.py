59d23d04018b914ea1801cd1e3611cda6ff0c0ac
jlswift2 <jlswift2@gmail.com>
2021-09-19 08:13:04 +0900

    remove redundant auto_now argument from start_time and end_time

--- reservations/models.py
+++ reservations/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 class Reservation(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    start_time = models.DateTimeField(auto_now=False)
-    end_time = models.DateTimeField(auto_now=False)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
     other_occupants = models.TextField()
     purpose = models.TextField()
