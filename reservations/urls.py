0795594085dfce4d5c8f4b3b246dba196d1a30c8
jlswift2 <jlswift2@gmail.com>
2021-10-17 10:38:52 +0900

    reinsert <int:pk> parameter

--- reservations/urls.py
+++ reservations/urls.py
@@ -5,6 +5,6 @@ from .apps import ReservationsConfig
 
 app_name = ReservationsConfig.name
 urlpatterns = [
-    path('details', ReservationDetails.as_view(), name='reservation_details'),
+    path('<int:pk>/details/', ReservationDetails.as_view(), name='reservation_details'),
     path('', ReservationList.as_view(), name='reservation_list'),
 ]
