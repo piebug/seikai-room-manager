1acaf1b01aa1c0c920ead83f405af3cddddbd46d
jlswift2 <jlswift2@gmail.com>
2021-10-19 23:07:21 +0900

    change url pattern name to match rooms current url naming conventions

--- reservations/urls.py
+++ reservations/urls.py
@@ -6,5 +6,5 @@ from .apps import ReservationsConfig
 app_name = ReservationsConfig.name
 urlpatterns = [
     path('<int:pk>/details/', ReservationDetails.as_view(), name='reservation_details'),
-    path('', ReservationList.as_view(), name='reservation_list'),
+    path('', ReservationList.as_view(), name='reservations_list'),
 ]
